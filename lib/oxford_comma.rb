8115b25cc39b8910eecac3c1ea9b13fc65583028
Tabitha Sin <tabitha.sin@gmail.com>
2017-10-13 13:30:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    last_element = array.pop()
+    new_array = array.join(", ")
+    new_array << (", and #{last_element}")
+    return new_array
+  end
 end
